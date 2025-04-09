f341fbab5cea627844d365878630f3dd83e56f47
Gabriel <agbalbuenaluna@gmail.com>
2025-04-09 13:10:29 -0600

    [VIEW] These is the basic structure for the view in a local MVVM architecture

--- app/src/main/java/com/example/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/view/MainActivity.kt
@@ -1,18 +1,30 @@
-package com.example.mvvm
+package com.example.mvvm.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import com.example.mvvm.databinding.ActivityMainBinding
+import com.example.mvvm.viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val quoteViewModel : QuoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        quoteViewModel.quoteModelLiveData.observe(this, Observer {
+            binding.tvQuote.text = it.quote
+            binding.tvAuthor.text = it.author
+        })
+
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
     }
 }
