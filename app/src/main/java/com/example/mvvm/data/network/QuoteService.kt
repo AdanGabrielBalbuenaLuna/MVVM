641345cd53c4dc05489262d600c7741312b38781
AdanGabrielBalbuenaLuna <agbalbuenaluna@gmail.com>
2025-07-31 17:43:04 -0600

    [DAGGER-HILT-Retrofit-Singleton] Configure Hilt for Singleton and Objects

--- app/src/main/java/com/example/mvvm/data/network/QuoteService.kt
+++ app/src/main/java/com/example/mvvm/data/network/QuoteService.kt
@@ -1,17 +1,18 @@
 package com.example.mvvm.data.network
 
-import com.example.mvvm.core.RetrofitHelper
 import com.example.mvvm.data.model.QuoteModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class QuoteService {
+class QuoteService @Inject constructor(private val quoteApiClient: QuoteApiClient) {
 
-    private val retrofit = RetrofitHelper.getRetrofit()
+    //private val retrofit = RetrofitHelper.getRetrofit()
 
     suspend fun getQuotes(): List<QuoteModel> {
         return withContext(Dispatchers.IO) {
-            val response = retrofit.create(QuoteApiClient::class.java).getAllQuotes()
+            //val response = retrofit.create(QuoteApiClient::class.java).getAllQuotes()
+            val response = quoteApiClient.getAllQuotes()
             response.body() ?: emptyList()
         }
     }
